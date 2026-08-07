46c2acd38dac3e5030dfc3785c8cba2a1723bfc0
kuwakuwa53577 <mikanm748@gmail.com>
2026-08-07 23:34:35 +0900

    Update preload.js

--- src/preload.js
+++ src/preload.js
@@ -1,8 +1,6 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  onTabsUpdated: (callback) => ipcRenderer.on('tabs-updated', (event, tabs, activeTabId) => callback(tabs, activeTabId)),
-  
   navigate: (url) => ipcRenderer.invoke('navigate', url),
   goBack: () => ipcRenderer.invoke('goBack'),
   goForward: () => ipcRenderer.invoke('goForward'),
@@ -11,22 +9,27 @@ contextBridge.exposeInMainWorld('electronAPI', {
   newTab: () => ipcRenderer.invoke('newTab'),
   switchTab: (id) => ipcRenderer.invoke('switchTab', id),
   closeTab: (id) => ipcRenderer.invoke('closeTab', id),
+  getTabs: () => ipcRenderer.invoke('getTabs'),
   reorderTabs: (ids) => ipcRenderer.invoke('reorder-tabs', ids),
-  
   setSidebarStatus: (isOpen) => ipcRenderer.invoke('set-sidebar-status', isOpen),
   setBookmarkBarStatus: (isOpen) => ipcRenderer.invoke('set-bookmark-bar-status', isOpen),
-  
   addBookmark: () => ipcRenderer.invoke('addBookmark'),
   getBookmarks: () => ipcRenderer.invoke('getBookmarks'),
-  removeBookmark: (url) => ipcRenderer.invoke('removeBookmark'),
+  removeBookmark: (url) => ipcRenderer.invoke('removeBookmark', url),
   getHistory: () => ipcRenderer.invoke('getHistory'),
   clearHistory: () => ipcRenderer.invoke('clearHistory'),
-
-  confirmDeleteBookmark: (title) => ipcRenderer.invoke('confirm-delete-bookmark', title),
-
-  openTabContextMenu: (tabId) => ipcRenderer.send('open-tab-context-menu', tabId),
+  openTabContextMenu: (id) => ipcRenderer.send('open-tab-context-menu', id),
   openBrowserUiContextMenu: () => ipcRenderer.send('open-browser-ui-context-menu'),
   minimize: () => ipcRenderer.send('window-minimize'),
   maximize: () => ipcRenderer.send('window-maximize'),
-  close: () => ipcRenderer.send('window-close')
+  close: () => ipcRenderer.send('window-close'),
+  onTabsUpdated: (callback) => ipcRenderer.on('tabs-updated', (event, tabs, activeId) => callback(tabs, activeId)),
+  
+  openBookmarkContextMenu: (bookmarkData) => ipcRenderer.send('open-bookmark-context-menu', bookmarkData),
+  onBookmarkDeleted: (callback) => ipcRenderer.on('bookmark-deleted-success', () => callback()),
+
+  togglePip: (url) => ipcRenderer.invoke('toggle-pip', url),
+  closePip: () => ipcRenderer.invoke('close-pip'),
+  movePip: (x, y) => ipcRenderer.invoke('move-pip', { x, y }),
+  resizePip: (width, height) => ipcRenderer.invoke('resize-pip', { width, height })
 });
